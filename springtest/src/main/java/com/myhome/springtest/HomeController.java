755bfca34a556f6ed6cdf9009668552c48e56eab
rndjhds <86414343+rndjhds@users.noreply.github.com>
2022-04-24 00:20:55 +0900

    Add files via upload

---
+++ springtest/src/main/java/com/myhome/springtest/HomeController.java
@@ -0,0 +1,53 @@
+package com.myhome.springtest;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private MyService service;
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/first", method = RequestMethod.GET)
+//	value="/"는 어떤값이 와도 된다는 의미이다.
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	@RequestMapping("second")
+	public String second(Model model) {
+		
+		model.addAttribute("test", "값전달 성공");
+		// model은 request의 역할을 대신해준다.
+		
+		return "result";
+	}
+	
+}
